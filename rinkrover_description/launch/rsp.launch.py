faf78dfa69bea1a1a9673ac925a1d32867506033
Kiran <kirank.suren@gmail.com>
2025-02-20 11:27:15 -0500

    update inertia, add ros2_control to urdf and udpate tricycle controller

--- rinkrover_description/launch/rsp.launch.py
+++ rinkrover_description/launch/rsp.launch.py
@@ -20,7 +20,7 @@ def generate_launch_description():
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     
-    # Create a robot_state_publisher node
+    # Create a robot_state_publisher node (transform tree)
     params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
