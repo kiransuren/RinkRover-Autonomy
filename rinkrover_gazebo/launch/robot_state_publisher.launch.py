433f14c4c9da7cd85d1e8dc2cfcb4edf57d5c8d7
Kiran <kirank.suren@gmail.com>
2025-02-20 12:01:01 -0500

    clean up old gazebo files and update with new urdf and controller (using ros2_control)

--- rinkrover_gazebo/launch/robot_state_publisher.launch.py
+++ rinkrover_gazebo/launch/robot_state_publisher.launch.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python3
-#
-# Copyright 2019 ROBOTIS CO., LTD.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Authors: Darby Lim
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
@@ -24,36 +6,34 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+import xacro
 
 def generate_launch_description():
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
-    urdf_file_name = 'rinkrover_base_model.urdf'
+    urdf_file_name = 'robot.urdf.xacro'
 
     print('urdf_file_name : {}'.format(urdf_file_name))
 
-    urdf_path = os.path.join(
-        get_package_share_directory('rinkrover_gazebo'),
-        'urdf',
-        urdf_file_name)
-
-    with open(urdf_path, 'r') as infp:
-        robot_desc = infp.read()
-
+    # Process the URDF file
+    pkg_path = os.path.join(get_package_share_directory('rinkrover_description'))
+    xacro_file = os.path.join(pkg_path,'description',urdf_file_name)
+    robot_description_config = xacro.process_file(xacro_file)
+    
+    # Create a robot_state_publisher node (transform tree)
+    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+    
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
+            description='Use sim time if true'),
 
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[{
-                'use_sim_time': use_sim_time,
-                'robot_description': robot_desc
-            }],
-        ),
+        node_robot_state_publisher
     ])
