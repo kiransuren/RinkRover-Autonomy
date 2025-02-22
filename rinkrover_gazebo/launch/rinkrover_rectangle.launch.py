6cfe278af08d0e47ec55b40e3186baa92f9952fb
Kiran <kirank.suren@gmail.com>
2025-02-22 12:07:57 -0500

    get gazebo simulation working

--- rinkrover_gazebo/launch/rinkrover_rectangle.launch.py
+++ rinkrover_gazebo/launch/rinkrover_rectangle.launch.py
@@ -6,13 +6,13 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     launch_file_dir = os.path.join(get_package_share_directory('rinkrover_gazebo'), 'launch')
-    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
-
+    
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     x_pose = LaunchConfiguration('x_pose', default='-2.0')
     y_pose = LaunchConfiguration('y_pose', default='-0.5')
@@ -24,18 +24,12 @@ def generate_launch_description():
     )
 
     gzserver_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
-        ),
-        launch_arguments={'world': world}.items()
-    )
-
-    gzclient_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
-        )
-    )
-
+            PythonLaunchDescriptionSource(
+                [PathJoinSubstitution([FindPackageShare('ros_gz_sim'),
+                                       'launch',
+                                       'gz_sim.launch.py'])]),
+            launch_arguments=[('ign_args', [f' -r -v4 {world}'])])
+    
     robot_state_publisher_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(launch_file_dir, 'robot_state_publisher.launch.py')
@@ -49,7 +43,7 @@ def generate_launch_description():
         ),
         launch_arguments={
             'x_pose': x_pose,
-            'y_pose': y_pose
+            'y_pose': y_pose,
         }.items()
     )
 
@@ -57,7 +51,6 @@ def generate_launch_description():
 
     # Add the commands to the launch description
     ld.add_action(gzserver_cmd)
-    ld.add_action(gzclient_cmd)
     ld.add_action(robot_state_publisher_cmd)
     ld.add_action(spawn_turtlebot_cmd)
 
--- rinkrover_gazebo/launch/spawn_rinkrover_robot.launch.py
+++ rinkrover_gazebo/launch/spawn_rinkrover_robot.launch.py
@@ -1,10 +1,9 @@
-import os
-
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import LaunchConfiguration, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
@@ -21,18 +20,65 @@ def generate_launch_description():
     declare_y_position_cmd = DeclareLaunchArgument(
         'y_pose', default_value='0.0',
         description='Specify namespace of the robot')
+    
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare('rinkrover_description'),
+            'config',
+            'tricycle_drive_controller.yaml',
+        ]
+    )
 
     start_gazebo_ros_spawner_cmd = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
+        package='ros_gz_sim',
+        executable='create',
         arguments=[
-            '-entity', 'rinkrover_robot',
+            '-name', 'rinkrover_robot',
             '-topic', 'robot_description',
+            '-allow_renaming', 'true',
             '-x', x_pose,
             '-y', y_pose,
-            '-z', '0.01'
+            '-z', '3'
         ],
         output='screen',
+    )
+    
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'joint_state_broadcaster',
+            '--param-file',
+            robot_controllers,
+            ],
+    )
+    tricycle_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'tricycle_controller',
+            '--param-file',
+            robot_controllers,
+            ],
+    )
+
+    # joint_state_broadcaster_spawner = ExecuteProcess(
+    #     cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+    #          'joint_state_broadcaster'],
+    #     output='screen'
+    # )
+
+    # tricycle_controller_spawner = ExecuteProcess(
+    #     cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+    #          'tricycle_controller'],
+    #     output='screen'
+    # )
+    # Bridge
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
+        output='screen'
     )
 
     ld = LaunchDescription()
@@ -41,7 +87,22 @@ def generate_launch_description():
     ld.add_action(declare_x_position_cmd)
     ld.add_action(declare_y_position_cmd)
 
+    ld.add_action(bridge)
+
     # Add any conditioned actions
     ld.add_action(start_gazebo_ros_spawner_cmd)
+    
+    ld.add_action(        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=start_gazebo_ros_spawner_cmd,
+                on_exit=[joint_state_broadcaster_spawner],
+            )
+        ))
+    ld.add_action(        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[tricycle_controller_spawner],
+            )
+        ))
 
     return ld
